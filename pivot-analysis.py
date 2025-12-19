422eb0ff1d1c95d202a4b545a52b1cc5d566b714
Leonard <Leonard.suares@gmail.com>
2025-12-19 16:16:37 -0500

    Analyzing the difference between assigned and reserved room type bookings

---
+++ pivot-analysis.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+import  chart_studio.plotly as py
+import plotly.graph_objs as go
+import plotly.express as px
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot
+
+# 1. Shows all columns (You already have this)
+pd.set_option('display.max_columns', None)
+
+# 2. DISABLES WRAPPING by setting the display width to a very high number
+pd.set_option('display.width', 1000)
+
+import  warnings
+from warnings import filterwarnings
+filterwarnings("ignore")
+
+df = pd.read_csv(r"C:\Users\leona\PycharmProjects\Python Data Analysis Projects\Hotel-bookings-analysis\hotel_bookings.csv")
+# print(df.head(5))
+
+filter1 = (df['children']==0) & (df['adults']==0) & (df['babies']==0)
+
+df2 = df[~filter1]
+# print(df2.shape)
+# print(df2.duplicated().sum())
+data = df2.drop_duplicates()
+# print(data.shape)
+
+# Analyzing the difference between assigned and reserved room type bookings
+pivot = pd.crosstab(index = data['reserved_room_type'], columns=data['assigned_room_type'], margins=True)
+# print(pivot)
+pivot_normalize = pd.crosstab(index = data['reserved_room_type'], columns=data['assigned_room_type'], margins=True, normalize='index').round(2)*100
+print(pivot_normalize)
+
